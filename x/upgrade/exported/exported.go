7d0a6315cdfc37a8045dc13c2607b252b050c14d
lsq984 <lsq984@163.com>
2026-04-30 10:33:43 +0800

    clear files

--- x/upgrade/exported/exported.go
+++
@@ -1,9 +0,0 @@
-package exported
-
-import sdk "github.com/cosmos/cosmos-sdk/types"
-
-// ProtocolVersionSetter defines the interface fulfilled by BaseApp
-// which allows setting it's appVersion field.
-type ProtocolVersionSetter interface {
-	SetAppVersion(sdk.Context, uint64)
-}
